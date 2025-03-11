78707c3903a2bb18a53f1b35205cc34cdc3ccfb0
ComputerWhiz24 <brunorollke@gmail.com>
2025-03-10 19:09:26 -0700

    Finished path finder method

--- src/AI/PathFinder.java
+++ src/AI/PathFinder.java
@@ -76,6 +76,110 @@ public class PathFinder {
 			if(gp.tileM.tile[tileNum].collision) {
 				node[col][row].solid = true;
 			}
+			// CHECK INTERACTIVE TILES 
+			for(int i = 0; i < gp.iTile[1].length; i++) {
+				if(gp.iTile[gp.currentMap][i] != null  && gp.iTile[gp.currentMap][i].destructible) {
+					int itCol = gp.iTile[gp.currentMap][i].worldX / gp.tileSize;
+					int itRow = gp.iTile[gp.currentMap][i].worldY / gp.tileSize;
+					node[itCol][itRow].solid = true;
+				}
+			}
+			// SET COST
+			getCost(node[col][row]);
+			col++;
+			if(col == gp.maxWorldCol) {
+				col = 0;
+				row++;
+			}
+		}
+	}
+	public void getCost(Node node) {
+		// G COST
+		int xDistance = Math.abs(node.col - startNode.col);
+		int yDistance = Math.abs(node.row - startNode.row);
+		node.gCost = xDistance + yDistance;
+		
+		// H COST
+		xDistance = Math.abs(node.col - goalNode.col);
+		yDistance = Math.abs(node.row - goalNode.row);
+		node.hCost = xDistance + yDistance;
+		
+		// F COST
+		 node.fCost = node.gCost+node.fCost;
+		 
+		 
+	}
+	public boolean search() {
+		
+		while(goalReached == false && step < 500) {
+			int col = currentNode.col;
+			int row = currentNode.row;
+			
+			// CHECK CURRENT NODE
+			currentNode.checked = true;
+			openList.remove(currentNode);
+			
+			// OPEN UP NODE
+			if(row - 1 >= 0) {
+				openNode(node[col][row-1]);
+			}
+			// OPEN LEFT NODE
+			if(col - 1 >= 0) {
+				openNode(node[col-1][row]);
+			}
+			// OPEN RIGHT NODE 
+			if(col + 1 <= gp.maxWorldCol) {
+				openNode(node[col+1][row]);
+			}
+			// OPEN DOWN NODE
+			if(row + 1 <= gp.maxWorldRow) {
+				openNode(node[col][row+1]);
+			}
+			
+			int bestNodeIndex = 0;
+			int bestNodefCost = 999;
+			
+			for(int i = 0; i < openList.size(); i++) {
+				// CHECK IF NODE'S FCOST IS BETTER
+				if(openList.get(i).fCost < bestNodefCost) {
+					bestNodeIndex = i;
+					bestNodefCost = openList.get(i).fCost;
+				} else if (openList.get(i).fCost == bestNodefCost) {
+					if(openList.get(i).gCost < openList.get(bestNodeIndex).gCost) {
+						bestNodeIndex = i;
+					}
+				}
+			}
+			
+			// IF NO NODE IS LEFT, END LOOP
+			if(openList.size() == 0) {
+				break;
+			}
+			currentNode = openList.get(bestNodeIndex);
+			
+			if(currentNode == goalNode) {
+				 goalReached = true;
+				 trackThePath();
+			}
+			step++;
+		}
+		return goalReached;
+	}
+	public void openNode(Node node) {
+		if(node.open == false && node.checked == false && node.solid == false) {
+			
+			node.open = true;
+			node.parent = currentNode;
+			openList.add(node);
+		}
+	}
+	public void trackThePath() {
+		Node current = goalNode;
+		
+		while(current != startNode) {
+			
+			pathList.add(0,current);
+			current = current.parent;
 		}
 	}
 }
